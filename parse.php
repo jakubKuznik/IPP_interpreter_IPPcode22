309299322b3d93eb2f8b17ffa03eab7eaff549bb
jakubKuznik <kuznja16@gmail.com>
2022-02-18 15:08:21 +0100

    Arg parse fixed

--- parse.php
+++ parse.php
@@ -13,7 +13,7 @@ ini_set('display_errors', 'stderr'); //Erross on stderr
     --jumps       // statistic - number or returns and jumps instruction\n");
 
 
-    $loc     = 0;   // Number of lines in code 
+    $loc     = 0;   // Number of lines, that has instruction in code 
     $coments = 0;   // Number of lines that has a comment 
     $labels  = 0;   // Number of labels in code 
     $jumps   = 0;   // Number of retuns and jumps in code 
@@ -24,12 +24,77 @@ ini_set('display_errors', 'stderr'); //Erross on stderr
     $h = parse_args($argc, $argv); 
     $stats_files  = $h[0];
     $stats_params = $h[1];
-
-    file_open_check($stats_files);
-    echo("gud");
+    if ($stats_files != NULL)
+        file_open_check($stats_files);
     
+    parse();
 
+    
+    /*      INSTRUCTION 
+        MOVE      CREATEFRAME   PUSHFRAME   POPFRAME   DEFVAR   CALL 
+        RETURN    PUSHS         POPS        ADD        SUB      MUL
+        IDIV      LT            GT          EQ         AND      OR
+        NOT       INT2CHAR      STRI2INT    READ       WRITE    CONCAT
+        STRLEN    GETCHAR       SETCHAR     TYPE       LABEL    JUMP
+        JUMPIFEQ  JUMPIFNEQ     EXIT        DPRINT     BREAK
+    */
+    /**
+     * Parse input program in ipp_code22 to array
+     * 
+     */
+    function parse(){
+        
 
+        while($line = fgets(STDIN)){
+            // todo wait for stdin input
+            // Remove whitespace 
+            echo($line);
+            $splitted = preg_split('/\s+/', trim($line, "\n"));
+            foreach ($splitted as $terminal){
+                echo($terminal . "\n");
+            }
+        }
+    }
+
+    /**
+     * control if each instruction is on new line;
+     * 
+     * return 1 if line has instruction 
+     * return 0 if line has no instruction 
+     * 
+     */
+    
+    function syntax_line_con(){
+
+    }
+
+    /*
+
+    while($line = fgets(STDIN)) {
+
+        // oddelene mista mezerami 
+        // split rozdeli podle mezer 
+        // trim da pryc newline  
+        $splitted = explode(' ', trim($line, "\n"));
+        foreach ($splitted as $terminal){
+            echo($terminal . "\n");
+        }
+    
+    }
+
+    //strto upper 
+    switch($splitted[0]){
+        case 'MOVE':
+            echo("dd");
+    }
+    
+    //strto upper 
+    switch($splitted[1]){
+        case 'MOVE':
+            echo("dd");
+    }
+
+*/
 
 
 
@@ -42,6 +107,9 @@ ini_set('display_errors', 'stderr'); //Erross on stderr
      * --coments     // statistic - number of lines that has a comment 
      * --labels      // statistic - number of labels in code     
      * --jumps       // statistic - number or returns and jumps instruction
+     * --fwjumps     // statistic - forward jumps 
+     * --backjumps   // statistic - back jumps 
+     * --badjumps    // statistic - jumps on nonexistig label
      * 
      * 
      *  example: 
@@ -54,8 +122,11 @@ ini_set('display_errors', 'stderr'); //Erross on stderr
      *  stats_param[1][0] = coments, stats_param[1][1] = lables    stats_param[1][2] = jumps 
      * 
      * 
-     * -- unknown parameter - error 10
-     * -- 
+     * -- unknown parameter           - error 10
+     * -- multiple write to same file - error 12
+     * 
+     * todo Pokus o zápis více skupin statistik
+     * do stejného souboru během jednoho spuštění skriptu vede na chybu 12. [1,5 b]
      * 
      * 
      * return array($stats_files, $stats_param)
@@ -65,8 +136,8 @@ ini_set('display_errors', 'stderr'); //Erross on stderr
         $stats          = false;   // Indicator if stats arg is there 
         $stats_arg_sum  = 0;       // How many stats args are there 
         $stats_sum      = 0;       // how manz stats files are there.
-        $s_f;                      // stats_file File where statisic will be writen 
-        $s_p;                      //  statas_param Parameters for eas statas 
+        $s_f = NULL;               // stats_file File where statisic will be writen 
+        $s_p = NULL;               //  statas_param Parameters for eas statas 
         $patern = "/^--stats=/";   // regex for --stats=filename
         
         foreach ($argv as $ar){
@@ -124,19 +195,66 @@ ini_set('display_errors', 'stderr'); //Erross on stderr
                 $s_p[$stats_sum-1][$stats_arg_sum] = 'jum';
                 $stats_arg_sum++;
             }
+            else if ($ar == '--fwjumps'){
+                if ($stats == false){
+                    fwrite(STDERR, "--stats=fileName has to be defined before\n");
+                    exit(10);
+                }
+                $s_p[$stats_sum-1][$stats_arg_sum] = 'fwj';
+                $stats_arg_sum++;
+            }
+            else if ($ar == '--backjumps'){
+                if ($stats == false){
+                    fwrite(STDERR, "--stats=fileName has to be defined before\n");
+                    exit(10);
+                }
+                $s_p[$stats_sum-1][$stats_arg_sum] = 'bac';
+                $stats_arg_sum++;
+            }
+            else if ($ar == '--badjumps'){
+                if ($stats == false){
+                    fwrite(STDERR, "--stats=fileName has to be defined before\n");
+                    exit(10);
+                }
+                $s_p[$stats_sum-1][$stats_arg_sum] = 'bad';
+                $stats_arg_sum++;
+            }
             else {
                 fwrite(STDERR, "UNKNOWN parameter\n");
                 exit(10);
             }
         }
+
+        if (array_duplicity($s_f) == TRUE){
+            fwrite(STDERR, "Error One file for multiple statistic\n");
+            exit(12);
+        }
         return array($s_f, $s_p);
     }
+
+    /**
+     * return True if array has duplicit node
+     * return False if not
+     * 
+     * for error 12  
+     */
+    function array_duplicity($arr){
+        for ($i = 0; $i < sizeof($arr); $i++){
+            for ($j = 0; $j < sizeof($arr); $j++){
+                if ($j == $i)
+                    continue;
+                if ($arr[$i] == $arr[$j])
+                    return TRUE;
+            }
+        }
+        return FALSE;
+    }
+
 
     /**
      * Check if stats file exist. 
      * 
      * error -12 --ouptu files cannot be open 
-     * //todo check absolute vs relative 
      * //todo filepath can contain unicode in UTF-8 
      */
     function file_open_check($files){
@@ -159,45 +277,4 @@ ini_set('display_errors', 'stderr'); //Erross on stderr
 
     
 
-/*
-// if ($argc > 1)
-// if ($argv[2] == 'neco ')
-
-    echo("kek");
-
-    while($line = fgets(STDIN)) {
-
-        // oddelene mista mezerami 
-        // split rozdeli podle mezer 
-        // trim da pryc newline  
-        $splitted = explode(' ', trim($line, "\n"));
-        foreach ($splitted as $terminal){
-            echo($terminal . "\n");
-        }
-    
-    }
-
-    echo(sum(2,5));
-
-
-    //strto upper 
-    switch($splitted[0]){
-        case 'MOVE':
-            echo("dd");
-    }
-    
-    //strto upper 
-    switch($splitted[1]){
-        case 'MOVE':
-            echo("dd");
-    }
-
-
-
-    exit(0);
-
-    function sum(int $a, int $b){
-        return $a + $b;
-    }
-*/
 ?>
