f933a8e7affc4e11cd9b6d5dd78c983b5605f09c
jakubKuznik <kuznja16@gmail.com>
2022-02-28 10:03:19 +0100

    Arg parse done

--- test.php
+++ test.php
@@ -18,16 +18,20 @@
     define ("ERR_HELP", "Cannot combine with other arguments\n");
     define ("ERR_COM", "Cannot combine --parse-only and --int-only\n");
     define ("ERR_NON_EXI_FILE", " File does not exists.\n");
+    define ("ERR_NON_EXI_DIR", " Directory does not exists.\n");
+    define ("ERR_BAD_REG", " Not an PCRE regex.\n");
 
     /***************** PROGRAM  *******************/
     $set = new Settings();
     
+    print_r($set);
     parse_args($argc, $argv, $set);
+    print_r($set);
     
     // check if $parse_path, $int_path, $jexam_path exist.
-
     $set->file_exist();
-
+    $set->dir_exist();
+    $set->regex_valid();
 
     /**
      * Script settings. 
@@ -37,13 +41,47 @@
         public $inter_on    = true;              // interpreter 
         public $recursive   = false;             // recursive folder                  
         public $noclean     = false;             // dont delete temp files 
+        public $test_list   = false;
         public $parse_path  = "parse.php";       // path to parser script 
         public $int_path    = "interpret.py";    // Path to interpreter script 
         public $jexam_path  = "/pub/courses/ipp/jexamxml/jexamxml.jar";
         public $match_regex = "";                // match regex files 
-        public $tests_files;                     // array of files and folders that we will test 
-
+        public $directory = ".";                 // for --directory= or --testlist=
         
+        public $tests_files;                     // array of files and folders that we will test 
+       
+        /**
+         * Check if $match_regex is valid PCRE regex
+         */
+        function regex_valid(){
+            if ($this->match_regex == "")
+                return;
+
+            preg_match($this->match_regex, 'foobar foobar foobar');
+            if (preg_last_error() !== PREG_NO_ERROR) {
+                fwrite(STDERR, $this->match_regex . ERR_BAD_REG); exit(41);
+            }
+        }
+
+        /**
+         * Check if $directory exist 
+         * 
+         * if $test_list = true => check if $directory is file 
+         * if $test_list = false => check if $directory is folder 
+         */
+        function dir_exist(){
+            if ($this->test_list == true){
+                if (file_exists($this->directory) == false){
+                    fwrite(STDERR, $this->directory . ERR_NON_EXI_FILE); exit(41);
+                }
+            }
+            else{
+                if (is_dir($this->directory) == false){
+                    fwrite(STDERR, $this->directory . ERR_NON_EXI_DIR); exit(41);
+                }
+            }
+        }
+
         /**
          * Check if $parse_path, $int_path $jexam_path exist 
          * exit(41) if not 
@@ -66,10 +104,7 @@
                 }
             }
         }
-        
     }
-
-
 
     function testing($tests_files){
         // if file rc != 0
@@ -115,9 +150,12 @@
      *  */    
     function parse_args(int $argc, array $argv, $set){ 
         
-        $int_only = false;
-        print_r($set);
-        
+        $int_only   = false; // cannot combine --int-only wiht --parse-only
+        $parse_only = false;
+        $directory  = false; // cannot 
+        $testlist   = false;
+
+     
         // two switch one for arguments with '='
         foreach ($argv as $arg){
             
@@ -131,33 +169,39 @@
                         if ($testlist == true || $directory == true){
                             fwrite(STDERR, ERR_HELP); exit(10);
                         }
-
                         $directory = true;
+                        $set->directory = $help_file_name;
                         break;
                     case "--parse-script":
                         if ($int_only == true){ //cannot combine with --int-only
                             fwrite(STDERR, ERR_COM); exit(10);
                         }
-                        $parse_path = $help_file_name;
+                        $parse_only = true;
+                        $set->parse_path = $help_file_name;
                         break;
                     case "--int-script":
                         if ($parse_only == true){
                             fwrite(STDERR, ERR_COM); exit(10);
                         }
-                        $int_path = $help_file_name;
+                        $set->int_path = $help_file_name;
+                        $int_only = true;
                         break;
                     case "--jexampath":      
                         if ($int_only == true){ //cannot combine with --int-only
                             fwrite(STDERR, ERR_COM); exit(10);
                         }
-                        $jexam_path = $help_file_name; 
+                        $parse_only = true;
+                        $set->jexam_path = $help_file_name; 
                         break;
                     case "--match":
+                        $set->match_regex = $help_file_name;
                         break;
                     case "--testlist":
                         if ($directory == true){
                             fwrite(STDERR, ERR_HELP); exit(10);
                         }
+                        $set->directory = $help_file_name;
+                        $set->test_list = true;
                         $testlist = true;
                         break;
                     default:
@@ -176,18 +220,20 @@
                         fwrite(STDERR, ERR_COM); exit(10);
                     } 
                     $parse_only = true;
-                    $inter_on   = false;  // disable interpreter test 
+                    $set->inter_on   = false;  // disable interpreter test 
                     break;
                 case "--int-only":
                     if ($parse_only == true){
                         fwrite(STDERR, ERR_COM); exit(10);
                     }
                     $int_only   = true;   
-                    $parser_on  = false;  // disable parser test 
+                    $set->parser_on  = false;  // disable parser test
                     break;
                 case "--recursive":
+                    $set->recursive = true;
                     break;
                 case "--noclean":
+                    $set->noclean = true;
                     break;
                 default:
                     break;
